06d6dd8b14cc9af871454a4e9f1635444579a5ea
foamyguy <foamyguy@gmail.com>
2025-07-18 11:11:01 -0500

    common pin configs

---
+++ src/common_dvi_hstx_pin_configs.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "drivers/dvhstx/dvhstx.hpp"
+
+using pimoroni::DVHSTXPinout;
+
+static const DVHSTXPinout adafruit_hstxdvibell_cfg = {14, 12, 18, 16};
+static const DVHSTXPinout adafruit_metro_rp2350_cfg = {14, 18, 16, 12};
+static const DVHSTXPinout adafruit_feather_rp2350_cfg = {14, 18, 16, 12};
+static const DVHSTXPinout adafruit_fruit_jam_cfg = {13, 15, 17, 19};
